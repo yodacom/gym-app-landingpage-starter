367191a70390d83323f9c4c51f656a048a163928
jb@yodacom.com <jb@yodacom.com>
2024-10-11 12:55:33 -0600

    commit Reviews FAQ

---
+++ src/components/FAQs.svelte
@@ -0,0 +1,13 @@
+<script>
+    import SectionWrapper from "./SectionWrapper.svelte";
+</script>
+
+<SectionWrapper id="reviews">
+    <div class="flex flex-col gap-10 sm:gap-14 md:gap-24 py-20 flex-1 items-center justify-center">
+        <div class="flex flex-col gap-2 sm:gap-14 md:gap-24">
+            <p class="opacity-60 text-base sm:text-lg md:text-xl text-center">For all your questions, qualms & queries </p>
+            <h3 class="text-4xl sm:text-5xl md:text-6xl max-w-[1000px] mx-auto w-full font-semibold text-center">Frequently Asked Questions
+            </h3>
+        </div>
+        </div>
+</SectionWrapper>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script>
     import Hero from "../components/Hero.svelte";
     import Product from "../components/Product.svelte";
+    import Reviews from "../components/Reviews.svelte";
+  import FaQs from "../components/FAQs.svelte";
 </script>
 
 <main  class="flex flex-col">
-    
     <Hero />
     <Product />
+    <Reviews />
+    <FaQs />
 </main>
