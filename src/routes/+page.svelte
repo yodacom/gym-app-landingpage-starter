84f7be4a9fb168b326ac4c0a7e7314bcf0dc9250
jb@yodacom.com <jb@yodacom.com>
2024-10-12 09:21:30 -0600

    faq complete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import Hero from "../components/Hero.svelte";
     import Product from "../components/Product.svelte";
     import Reviews from "../components/Reviews.svelte";
-  import FaQs from "../components/FAQs.svelte";
+    import FaQs from "../components/FAQs.svelte";
 </script>
 
 <main  class="flex flex-col">
