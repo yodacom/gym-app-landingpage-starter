a7e8c65757ab18deda57ef8a794c308c23b3c9c9
jb@yodacom.com <jb@yodacom.com>
2024-10-13 16:52:00 -0600

    corrected jsconfig.json

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -63,11 +63,10 @@
             </button>
         </div>
         <div class="flex flex-col items-center justify-center">
-            <CtAs />
+            <CtAs dark={true} />
         </div>
     </div>
 {/if}
-``
 {#if y > outerHeight}
     <div class="bg-white fixed top-0 left-0 w-full flex flex-col z-20 px-4 fadeIn">
         <Header/>
